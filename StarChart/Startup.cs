32ba2b144d34e8bdd0fa2971a213105e4be69f49
rbacordio <113413337+rbacordio@users.noreply.github.com>
2022-09-16 10:03:40 +0800

    Added MVC Services

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -13,13 +13,12 @@ namespace StarChart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
-
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvc();
         }
     }
 }
